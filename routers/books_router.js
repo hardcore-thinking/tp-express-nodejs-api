2b198f58303d552074ff0256e0d3fcf3c8216ee5
Alexy MANSUY <alexymansuy31@gmail.com>
2024-01-04 13:49:49 +0100

    Updated books service and router

---
+++ routers/books_router.js
@@ -0,0 +1,17 @@
+const booksService = require('../services/available_books').books;
+
+exports.getBooks = function getBooks() {
+
+}
+
+exports.getBookById = function getBookById() {
+    
+}
+
+exports.addBook = function addBook() {
+    
+}
+
+exports.deleteBookById = function deleteBookById() {
+    
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const booksController = require('');
 
 const PORT = 8080;
 
