ecc3ff6e095f864a6f45fc7669beb0f266daf6e4
Alexy MANSUY <alexymansuy31@gmail.com>
2024-01-04 12:32:27 +0100

    Added books router, controller and service

---
+++ services/available_books.js
@@ -0,0 +1,37 @@
+const books = [
+    {
+        id: 0,
+        name: "Harry Potter à l'école des sorciers",
+        release_year: 1997
+    },
+    {
+        id: 1,
+        name: "Harry Potter et la Chambre des secrets",
+        release_year: 1998
+    },
+    {
+        id: 2,
+        name: "Harry Potter et le Prisonnier d'Azkaban",
+        release_year: 1999
+    },
+    {
+        id: 3,
+        name: "Harry Potter et la Coupe de feu",
+        release_year: 2000
+    },
+    {
+        id: 4,
+        name: "Harry Potter et l'Ordre du phénix",
+        release_year: 2003
+    },
+    {
+        id: 5,
+        name: "Harry Potter et le Prince de sang-mêlé",
+        release_year: 2005
+    },
+    {
+        id: 6,
+        name: "Harry Potter et les Reliques de la Mort",
+        release_year: 2007
+    }
+]
